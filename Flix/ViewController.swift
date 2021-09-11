ce288772746799d96c7c556884aa5ece09eee9e6
Sabrina Camargo <scama030@fiu.edu>
2021-09-11 12:34:46 -0400

    Create ViewController.swift

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Mohammad Shanti on 9/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
